28ce2ed9fb7a55a7a0fa660ff84524e80cdb8648
krishlala <71400038+krishlala@users.noreply.github.com>
2021-09-11 15:20:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
+name = document.getElementById("name").value;
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX=0;
+leftWristX=0;
 function setup() {
     video = createCapture(VIDEO);
-    video.size(550,550);
+    video.size(550, 550);
+
     canvas = createCanvas(550, 550);
+    canvas.position(560,150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose' ,gotPoses);
-
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-    background('aqua');
+    document.getElementById("square_side").innerHTML = "Width and Height of the square is = " + difference + "px";
+    background('#ADD8E6');
+    textSize(difference);
+    fill("#22E496");
+    text(name, 50, 400);
 }
 
 function modelLoaded() {
-    console.log('PoseNet model Loaded!');
+    console.log('PoseNet is Initialized!');
 }
 
 function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWrist = " + leftWristX + "rightWristX" + rightWristX + "difference = " + difference);
+
     }
 }
